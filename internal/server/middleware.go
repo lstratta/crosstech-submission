4ebf262a0571758b1aa254ea77a2d4a9e7091e64
Luke Stratta <mrlukestratta@proton.me>
2025-02-18 14:37:52 +0000

    add allowed method and headers

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -6,12 +6,14 @@ import (
 
 func (s *Server) middleware() {
 	s.router.Use(
-		middleware.Logger(),
-		middleware.Recover(),
 		middleware.CORSWithConfig(
 			middleware.CORSConfig{
 				AllowOrigins: s.conf.AllowedOrigins,
+				AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+				AllowHeaders: []string{"Accept", "Authorisation", "Content-Type", "X-CSRF-Token"},
 			},
 		),
+		middleware.Logger(),
+		middleware.Recover(),
 	)
 }
